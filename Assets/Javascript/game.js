0782494a1acc3293906b7fddb1dd2d5cb555f456
Desiree Tien <desireetien@Aphesis.local>
2017-11-15 14:42:19 -0600

    Updating

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -4,32 +4,23 @@ var targetNumber;
 var counter = 0;
 var gemValues = [];
 
-//array with four elements to run for loop four times
 var gemQuantity = [1, 2, 3, 4];
 
 
-// The player will be shown a random number at the start of the game.
 targetNumber = Math.floor((Math.random() * 101) + 19);
 $("#targetNumb").text(targetNumber);
 
 $("#Total").text(counter);
 
-// Generate four random numbers
 for (var i = 0; i < gemQuantity.length; i++) {
 	var newValue = Math.floor((Math.random() * 11) + 1);
 	gemValues.push(newValue);
 }
 
-//Assign one random number to each crystal
-// $("#blue").attr("data-rupeeValue", rupeeValues[0]);
-// $("#white").attr("data-rupeeValue", rupeeValues[1]);
-// $("#red").attr("data-rupeeValue", rupeeValues[2]);
-// $("#purple").attr("data-rupeeValue", rupeeValues[3]);
 $(".gemImage").each(function(index, item){
 	$(item).attr("data-gem-value", gemValue[index]);
 });
 
-// When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
 $(".gemImage").on("click", function() {
 	var singleGemValue = $(this).attr("data-gem-value");
 	singleGemValue = parseInt(singleGemValue)
